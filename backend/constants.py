b0848034f29821c365b907645100c2e9011ef5c8
andreapaci <andrea.paci1998@gmail.com>
2021-05-08 04:04:52 +0200

    refactor

---
+++ backend/constants.py
@@ -0,0 +1,3 @@
+# File contenetente costanti dell'applicativo
+
+MAX_MATRIX_SIZE = 8    # Dimensione massima della matrice
--- constants.py
+++
@@ -1,3 +0,0 @@
-# File contenetente costanti dell'applicativo
-
-MAX_MATRIX_SIZE = 10    # Dimensione massima della matrice (motivazioni computazionali e grafiche)
